b6bb22a96a7f889c4c02eb7918d7a01b1a461fcb
foglii <100421246+foglii@users.noreply.github.com>
2024-04-24 01:13:46 +0200

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 while true; do
     duration=$((RANDOM % 10 + 1))  # Random duration between 1 and 10 seconds
-    sleep_time=$((RANDOM % 5 + 2))   # Random sleep time between 0 and 5 seconds
-    echo "Sending TCP traffic for $duration seconds. Next transmission in $sleep_time seconds..."
+    sleep_time=$((RANDOM % 5 + 5))   # Random sleep time between 5 and 10 seconds
+    total_sleep=$((sleep_time + duration))
+    echo "Sending TCP traffic for $duration seconds. Next transmission in $total_sleep seconds..."
+    echo "Sending TCP traffic for $duration seconds. Next transmission in $total_sleep seconds..." > output.txt
     #h1=$(hostname -I | awk '{print $1}')
     #h2=$(hostname -I | awk '{print $2}')
     #h1_ip=$(h1 ifconfig h1-eth0 inet )
     iperf -c 10.0.0.1 -t $duration
-    total_sleep=$((sleep_time + duration))
     sleep $total_sleep
+    
 done
